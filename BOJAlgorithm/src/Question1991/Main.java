b9074347797cc9d0646fd6c13d4f9fff44f1c26d
Jaeyong <koola97620@nate.com>
2018-11-03 08:31:41 +0900

    03/11/18
     문제

--- BOJAlgorithm/src/Question1991/Main.java
+++ BOJAlgorithm/src/Question1991/Main.java
@@ -1,5 +1,7 @@
 package Question1991;
 
 public class Main {
+    public static void main(String[] args) {
 
+    }
 }
--- BOJAlgorithm/src/Question5639/Main.java
+++ BOJAlgorithm/src/Question5639/Main.java
@@ -1,4 +1,7 @@
 package Question5639;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
