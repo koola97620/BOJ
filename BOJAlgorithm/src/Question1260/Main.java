e6bfa35978cb5f29ea6725a342b1e6120ab29823
Jaeyong <koola97620@nate.com>
2018-11-09 17:47:03 +0900

    09/11/18
     새문제.

--- BOJAlgorithm/src/Question1260/Main.java
+++ BOJAlgorithm/src/Question1260/Main.java
@@ -5,4 +5,7 @@ package Question1260;
  */
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- BOJAlgorithm/src/Question5052/Main.java
+++ BOJAlgorithm/src/Question5052/Main.java
@@ -1,4 +1,10 @@
 package Question5052;
 
+/*
+    5052번  전화번호 목록.
+ */
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
