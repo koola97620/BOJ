441543f13fc4144d1d39282258ed49031bf343d0
Jaeyong <koola97620@nate.com>
2018-11-10 17:48:05 +0900

    10/11/18
     새문제.

--- BOJAlgorithm/src/Question1260/Main.java
+++ BOJAlgorithm/src/Question1260/Main.java
@@ -4,8 +4,12 @@ package Question1260;
 1260번 DFS 와 BFS
  */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     }
 }
--- BOJAlgorithm/src/Question5052/Main.java
+++ BOJAlgorithm/src/Question5052/Main.java
@@ -1,10 +1,13 @@
 package Question5052;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /*
     5052번  전화번호 목록.
  */
 public class Main {
     public static void main(String[] args) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
